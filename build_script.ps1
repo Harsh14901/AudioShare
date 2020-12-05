d03f175bd3585b805c12da8c46ee9ceb1c571093
saptarshi <2001saptarshi@gmail.com>
2020-12-06 01:42:12 +0530

    packaging of Server and CLI updated for windows

---
+++ build_script.ps1
@@ -0,0 +1,28 @@
+mkdir -p .cache
+
+python.exe -m venv venv
+.\venv\Scripts\Activate.ps1
+cd CLI
+pip install -r requirements.txt
+pip install -r dev-requirements.txt
+	
+cd ..\Server
+npm ci 
+npm run create-executable
+Move-Item .\CAV_server.exe ..\.cache -force
+	
+cd ..\CLI\cli
+pyinstaller -F --add-data '..\..\.cache\CAV_server.exe;.' .\main.py -n LocalParty
+Move-Item .\dist\LocalParty.exe ..\..\.cache\ -force
+
+cd ..\..
+mkdir -p .\Electron\ElectronGUI\binaries\
+Copy-Item .cache\LocalParty .\Electron\ElectronGUI\binaries\ -force
+cd Electron\ElectronGUI\
+npm ci
+npm run make
+mv out\ ..\..\dist\
+	
+rm -rf .cache\ 
+cd CLI\cli
+rm -rf build\ LocalParty.spec;
--- makefile
+++ makefile
@@ -1,17 +1,15 @@
-.ONESHELL:
-all:
-	cd Server
-	npm run create-executable
-	yes | mv CAV_server ../CLI/cli/
-	
-	cd ../CLI/cli/
-	make
-	cd ../../Electron/ElectronGUI
-	
-	mkdir -p binaries
-	yes | cp ../../CLI/cli/dist/LocalParty binaries/
-	npm run make
-	cd ../../
-	mkdir -p out
-	mv Electron/ElectronGUI/out/ ./
+cd Server
+npm run create-executable
+mv CAV_server ..\CLI\cli\
+
+cd ..\CLI\cli\
+make
+cd ..\..\Electron\ElectronGUI
+
+mkdir -p binaries
+cp ..\..\CLI\cli\dist\LocalParty binaries\
+npm run make
+cd ..\..\
+mkdir -p out
+mv Electron\ElectronGUI\out\ .\
 
